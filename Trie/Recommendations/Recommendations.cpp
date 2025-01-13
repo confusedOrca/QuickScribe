0ed56f61c85242d765f904ec4a3ef96cd350669b
Al Ibne Siam <confusedorca009@gmail.com>
2025-01-13 20:06:01 +0600

    implemented recommendations

---
+++ Trie/Recommendations/Recommendations.cpp
@@ -0,0 +1,39 @@
+#include "Recommendations.h"
+#include <stdexcept>
+#include <iostream>
+
+Recommendations::Recommendations(int maxSize) : maxSize(maxSize) {
+    if (maxSize <= 0) throw std::invalid_argument("maxSize must be greater than 0.");
+}
+
+void Recommendations::addWord(std::string word, int frequency) {
+    if (wordSet.count(word)) throw std::invalid_argument("Word already exists.");
+
+    if (frequency <= 0) throw std::invalid_argument("Frequency must be greater than 0.");
+
+    if (!recommendations.empty() && frequency < recommendations.top().frequency) return;
+    
+    Recommendation newRec{word, frequency};
+    recommendations.push(newRec);
+    wordSet.insert(word);
+
+    if (recommendations.size() > maxSize) {
+        Recommendation minRec = recommendations.top();
+        recommendations.pop();
+        wordSet.erase(minRec.word);
+    }
+}
+
+
+std::vector<std::string> Recommendations::getRecommendations() {
+    std::vector<std::string> result(recommendations.size());
+    std::priority_queue<Recommendation, std::vector<Recommendation>, std::greater<Recommendation>> tempHeap = recommendations;
+    for (int i = result.size() - 1; i >= 0; --i) {
+        result[i] = tempHeap.top().word; 
+        tempHeap.pop();
+    }
+
+    return result;
+}
+
+
---
+++ Trie/Recommendations/Recommendations.h
@@ -0,0 +1,69 @@
+#ifndef RECOMMENDATIONS_H
+#define RECOMMENDATIONS_H
+
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <queue>
+#include <functional>
+
+/**
+ * @class Recommendations
+ * @brief Maintains a dynamic list of the most frequent words upto maxSize.
+ */
+class Recommendations {
+private:
+    /**
+     * @struct Recommendation
+     * @brief Represents a single word recommendation along with its frequency.
+     * overridden operators ensures comparison is based on 
+     */
+    struct Recommendation {
+        std::string word;
+        int frequency;
+
+        bool operator<(const Recommendation& other) const {
+            return frequency < other.frequency;
+        }
+
+        bool operator>(const Recommendation& other) const {
+            return frequency > other.frequency; 
+        }
+
+        bool operator==(const Recommendation& other) const {
+            return word == other.word;
+        }
+    };
+
+    std::unordered_set<std::string> wordSet;  ///< Tracks words already added to recommendations.
+    std::priority_queue<Recommendation, std::vector<Recommendation>, std::greater<Recommendation>> recommendations;///< Min heap: stores recommendations in sorted order.
+    int maxSize;  ///< Maximum allowed size of the recommendations list.
+
+public:
+    /**
+     * @brief Constructor to initialize the Recommendations object.
+     * @param maxSize The maximum size of the recommendations.
+     * @throws invalid_argument if `maxSize` is less than or equal to 0.
+     */
+    Recommendations(int maxSize);
+
+    /**
+     * @brief Adds new words to the recommendations list, maintaining the order of frequency.
+     * If frequency <  min frequency (root frequency): returns
+     * Else inserts new word into recommendations heap.
+     * If size > maxSize removes minimum frequency element by popping root.
+     * @param word The word to be added to the recommendations list.
+     * @param frequency The frequency of the word to be added.
+     * @throws std::invalid_argument if the word already exists or if frequency <= 0.
+     */
+    void addWord(std::string word, int frequency);
+
+    /**
+     * @brief Retrieves recommendations words in descending order of frequency.
+     * Inserts words from last idx to start by repeatedly popping the root.
+     * @return A vector of strings representing the recommended words.
+     */
+    std::vector<std::string> getRecommendations();
+};
+
+#endif // RECOMMENDATIONS_H

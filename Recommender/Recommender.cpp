72e967ee91356b6486c06858aca630e598c8e72d
Al Ibne Siam <confusedorca009@gmail.com>
2025-01-14 14:28:58 +0600

    restored accidentally removed insert function

--- Recommender/Recommender.cpp
+++ Recommender/Recommender.cpp
@@ -9,6 +9,30 @@ Recommender::Recommender() {
     trieRoot = new Node();
 }
 
+void Recommender::insert(const std::string& word) {
+    Node* curr = trieRoot;
+    for (char c : word) {
+        if (curr->children.find(c) == curr->children.end()) {
+            curr->children[c] = new Node();
+        }
+        curr = curr->children[c];
+    }
+    curr->wordFrequency++;
+}
+
+std::vector<std::string> Recommender::recommend(const std::string& prefix, int n) {
+    Node* curr = trieRoot;
+    for (char c : prefix) {
+        if (curr->children.find(c) == curr->children.end()) return {};
+        curr = curr->children[c];
+    }
+
+    Recommendations recommendations(n + 1);
+    recommendations.addWord(prefix, INFINITY_INT);
+    collectWords(curr, prefix, recommendations);
+    return recommendations.getRecommendations();
+}
+
 void Recommender::collectWords(Node* node, const std::string& prefix, Recommendations& recommendations) {
     std::vector<std::future<void>> futures;
     std::mutex mtx;
@@ -29,26 +53,5 @@ void Recommender::collectWords(Node* node, const std::string& prefix, Recommenda
 
     for (auto& future : futures) {
         future.get();
-    }
-}
-
-std::vector<std::string> Recommender::recommend(const std::string& prefix, int n) {
-    Node* curr = trieRoot;
-    for (char c : prefix) {
-        if (curr->children.find(c) == curr->children.end()) return {};
-        curr = curr->children[c];
-    }
-
-    Recommendations recommendations(n + 1);
-    recommendations.addWord(prefix, INFINITY_INT);
-    collectWords(curr, prefix, recommendations);
-    return recommendations.getRecommendations();
-}
-
-void Recommender::collectWords(Node* node, const std::string& prefix, Recommendations& recommendations) {
-    if (node->wordFrequency > 0) recommendations.addWord(prefix, node->wordFrequency);
-
-    for (auto& [character, nextNode] : node->children) {
-        collectWords(nextNode, prefix + character, recommendations);
     }
 }
